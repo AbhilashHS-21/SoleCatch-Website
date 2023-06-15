c982ecb1eb854d750378e1c97e74f2b1591ce65d
AbhilashHS-21 <abhilash2k21@gmail.com>
2023-06-15 13:40:05 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+const navElems = [overlay, navOpenBtn, navCloseBtn];
+
+for (let i = 0; i < navElems.length; i++) {
+  navElems[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+// back to top button
+
+const header = document.querySelector("[data-header]");
+const goToTop = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (this.window.scrollY >= 80) {
+    header.classList.add("active");
+    goToTop.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goToTop.classList.remove("active");
+  }
+});
